d760952d7f541a943fbe96aa9a67a32245a40ee1
Aziz <shaqhuseynov@gmail.com>
2020-04-08 01:07:41 +0200

    Updates

---
+++ precache-manifest.b35be17ca4707ee47f925471bcd40fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59db4ac0b66486050bc1d88a8ec574a6",
+    "url": "/fuzzylogic/index.html"
+  },
+  {
+    "revision": "1ee3f5b7002d55ceb114",
+    "url": "/fuzzylogic/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d13ca5732c1f7f444006",
+    "url": "/fuzzylogic/static/css/main.5e69781a.chunk.css"
+  },
+  {
+    "revision": "1ee3f5b7002d55ceb114",
+    "url": "/fuzzylogic/static/js/2.02730113.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fuzzylogic/static/js/2.02730113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13ca5732c1f7f444006",
+    "url": "/fuzzylogic/static/js/main.ababa6fa.chunk.js"
+  },
+  {
+    "revision": "63931bc6b1af4b5f64f0",
+    "url": "/fuzzylogic/static/js/runtime-main.4c46f7e4.js"
+  }
+]);
